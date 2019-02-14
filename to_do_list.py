aaa2eba5e8e2c577f83156d31f4230ff8a3523b4
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2019-02-14 10:50:03 -0600

    Update to_do_list.py

--- to_do_list.py
+++ to_do_list.py
@@ -1,6 +1,6 @@
 print("Hello this application is designed to keep a To-Do list")
 print(" it will allow you to continually add new items just press enter")
-print("  * To quit enter \":wq \" * ")
+print("  * To quit enter \" :wq \" * ")
 
 count = 1                 # the counter for the print out
 

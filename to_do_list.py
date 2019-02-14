d993cf25253b44a7f93cd6dfe4950e525a01274c
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2019-02-14 10:49:23 -0600

    Create to_do_list.py

---
+++ to_do_list.py
@@ -0,0 +1,15 @@
+print("Hello this application is designed to keep a To-Do list")
+print(" it will allow you to continually add new items just press enter")
+print("  * To quit enter \":wq \" * ")
+
+count = 1                 # the counter for the print out
+
+while 1 < 2:              # this condition will always be true
+
+    print(count, ")")     # print out for displaying counter
+    my_list = input()     # allows for the continued input
+    print("\n")
+    int(count)            # turns the counter back into and integer value so it can be incremented
+    count += 1
+    if my_list == ":wq":  # if the user enters :wq (write and quit) then end then break the while loop
+        break

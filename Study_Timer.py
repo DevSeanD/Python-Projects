f25d53f020556b95d65fc5609f8917df0400f197
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2019-03-07 10:54:38 -0500

    Reformated window size and spacing

--- Study_Timer.py
+++ Study_Timer.py
@@ -1,9 +1,7 @@
 """
 Hello this application is designed as a study timer app to aid the user when studying by keeping them motivated
 This application uses tkinter as its GUI interface
-
 Created by: DevSeanD
-
 """
 
 import tkinter as tk
@@ -15,56 +13,52 @@ window = tk.Tk()
 
 window.title("Study Timer")
 
-window.geometry("600x400")
+window.geometry("450x450")
+
 
 # Functions
 
 
-def study_timer():              # does time conversions
+def study_timer():  # does time conversions
 
     num_hour = float(entry_field1.get())
     num_hour = num_hour * 3600  # converts hours to seconds
 
     num_min = float(entry_field2.get())
-    num_min = num_min * 60      # converts minuets to seconds
+    num_min = num_min * 60  # converts minuets to seconds
 
     total_sec = num_min + num_hour
 
-    return total_sec        # prints out a motivation quote dependant upon random generated number
+    return total_sec  # prints out a motivation quote dependant upon random generated number
 
 
-for y in range(1):          # generates one random number
+for y in range(1):  # generates one random number
     quote = (random.randint(1, 5))  # generates a random number between 1 and 5
 
     label_space = tk.Label(text=" ")
-        
+
     if quote == 1:
-        label_space.grid(column=0,row=5)
-        label_quote1 = tk.Label(text="  Never, never, never give up                                          ")
+        label_quote1 = tk.Label(text="  \"Never, never, never give up\"                                        ")
         label_quote1.grid(column=0, row=6)
 
     if quote == 2:
-        label_space.grid(column=0, row=5)
-        label_quote2 = tk.Label(text="  Quality is not an act, it is a habit                                 ")
+        label_quote2 = tk.Label(text="  \"Quality is not an act, it is a habit\"                                ")
         label_quote2.grid(column=0, row=6)
 
     if quote == 3:
-        label_space.grid(column=0, row=5)
-        label_quote2 = tk.Label(text="  It always seems impossible until its done                            ")
+        label_quote2 = tk.Label(text="  \"It always seems impossible until its done\"                            ")
         label_quote2.grid(column=0, row=6)
 
     if quote == 4:
-        label_space.grid(column=0, row=5)
-        label_quote2 = tk.Label(text="  Life is 10% what happens to you and 90% how you react to it")
+        label_quote2 = tk.Label(text="  \"Life is 10% what happens to you and 90% how you react to it\"")
         label_quote2.grid(column=0, row=6)
 
     if quote == 5:
-        label_space.grid(column=0, row=5)
-        label_quote2 = tk.Label(text="  You cannot cross the sea by merely staring at the water")
+        label_quote2 = tk.Label(text="  \"You cannot cross the sea by merely staring at the water\"")
         label_quote2.grid(column=0, row=6)
 
 
-def display_timer():            # displays the ending message
+def display_timer():  # displays the ending message
 
     time.sleep(study_timer())
     display_min = float(study_timer() / 60)
@@ -82,28 +76,24 @@ def display_timer():            # displays the ending message
 
 
 # LABEL
-title = tk.Label(text="Hello welcome to Study Timer", font=("Times New Roman", 15))
+title = tk.Label(text=" ")
 title.grid(column=0, row=0)
 
 label_hour = tk.Label(text="How many hours?")
 label_hour.grid(column=0, row=1)
 
 label_min = tk.Label(text="How many minuets?")
-label_min.grid(column=0, row=2)
-
+label_min.grid(column=0, row=3)
 
 # BUTTON
 button1 = ttk.Button(text="Start", command=display_timer)
-button1.grid(column=1, row=3)
+button1.grid(column=0, row=5)
 
 # Entry Field
 entry_field1 = ttk.Entry()
-entry_field1.grid(column=1, row=1)
+entry_field1.grid(column=0, row=2)
 
 entry_field2 = ttk.Entry()
-entry_field2.grid(column=1, row=2)
-
+entry_field2.grid(column=0, row=4)
 
 window.mainloop()
-
-

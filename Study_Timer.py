ef105938e860527ee611bd9e14e56ba858b94827
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2019-03-07 10:04:41 -0500

    Shortened for efficiency
    Removed multiple initiations of label_space

--- Study_Timer.py
+++ Study_Timer.py
@@ -9,7 +9,7 @@ Created by: DevSeanD
 import tkinter as tk
 import time
 import random
-
+from tkinter import ttk
 
 window = tk.Tk()
 
@@ -34,34 +34,31 @@ def study_timer():              # does time conversions
 
 
 for y in range(1):          # generates one random number
-    quote = (random.randint(1, 4))
+    quote = (random.randint(1, 5))  # generates a random number between 1 and 5
 
+    label_space = tk.Label(text=" ")
+        
     if quote == 1:
-        label_space = tk.Label(text=" ")
         label_space.grid(column=0,row=5)
         label_quote1 = tk.Label(text="  Never, never, never give up                                          ")
         label_quote1.grid(column=0, row=6)
 
     if quote == 2:
-        label_space = tk.Label(text=" ")
         label_space.grid(column=0, row=5)
         label_quote2 = tk.Label(text="  Quality is not an act, it is a habit                                 ")
         label_quote2.grid(column=0, row=6)
 
     if quote == 3:
-        label_space = tk.Label(text=" ")
         label_space.grid(column=0, row=5)
         label_quote2 = tk.Label(text="  It always seems impossible until its done                            ")
         label_quote2.grid(column=0, row=6)
 
     if quote == 4:
-        label_space = tk.Label(text=" ")
         label_space.grid(column=0, row=5)
         label_quote2 = tk.Label(text="  Life is 10% what happens to you and 90% how you react to it")
         label_quote2.grid(column=0, row=6)
 
     if quote == 5:
-        label_space = tk.Label(text=" ")
         label_space.grid(column=0, row=5)
         label_quote2 = tk.Label(text="  You cannot cross the sea by merely staring at the water")
         label_quote2.grid(column=0, row=6)
@@ -72,7 +69,6 @@ def display_timer():            # displays the ending message
     time.sleep(study_timer())
     display_min = float(study_timer() / 60)
 
-    label_space = tk.Label(text=" ")
     label_space.grid(column=0, row=7)
     label_fin = tk.Label(text="You have finished your study session of")
     label_fin.grid(column=0, row=8)
@@ -97,14 +93,14 @@ label_min.grid(column=0, row=2)
 
 
 # BUTTON
-button1 = tk.Button(text="Start", command=display_timer)
+button1 = ttk.Button(text="Start", command=display_timer)
 button1.grid(column=1, row=3)
 
 # Entry Field
-entry_field1 = tk.Entry()
+entry_field1 = ttk.Entry()
 entry_field1.grid(column=1, row=1)
 
-entry_field2 = tk.Entry()
+entry_field2 = ttk.Entry()
 entry_field2.grid(column=1, row=2)
 
 

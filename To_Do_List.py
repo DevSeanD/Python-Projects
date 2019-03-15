1b0e49915522c84c1783542a5c5c813b9bb94ee3
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2019-03-15 18:27:34 -0400

    Added a GUI using Tkinter
    Took the old program and refactored it for a GUI enviornment

--- To_Do_List.py
+++ To_Do_List.py
@@ -1,23 +1,55 @@
 """
-Hello, this program is a simple to do list. It works by allowing the user to input items and hit enter to create a new line
-indefinitley 
-
-Created by: DevSeanD
+Hello, this application is a simplistic to do list made using tkinter
+Create by: DevSeanD
 
 """
 
-print("Hello this application is designed to keep a To-Do list")
-print(" it will allow you to continually add new items just press enter")
-print("  * To quit enter \" :wq \" * ")
+import tkinter as tk
+import time
+import random
+from tkinter import ttk
+from tkinter import *
 
-count = 1                 # the counter for the print out
 
-while 1 < 2:              # this condition will always be true
+def to_do():
+    item_number = item_number_entry.get()
+    item_number = int(item_number)
+    counter = 1
+    y_shift = 0
 
-    print(count, ")")     # print out for displaying counter
-    my_list = input()     # allows for the continued input
-    print("\n")
-    int(count)            # turns the counter back into and integer value so it can be incremented
-    count += 1
-    if my_list == ":wq":  # if the user enters :wq (write and quit) then end then break the while loop
-        break
+    while True:
+        if counter != 0:
+            to_do_item = ttk.Entry(width=65)
+            to_do_item.place(x=30, y=110 + y_shift)
+            counter += 1
+            y_shift += 50
+
+        if counter > item_number:
+            break
+
+
+window = tk.Tk()
+
+window.title("To Do List")
+
+window.geometry("600x1000")
+
+# Label
+welcome_label = Label(text="Hello, welcome to a simple To-Do list app")
+welcome_label.place(x=150, y=10)
+
+item_number_label = Label(text="How many items would you like?")
+item_number_label.place(x=30, y=30)
+
+#   Entry
+item_number_entry = tk.Entry()
+item_number_entry.place(x=30, y=50)
+
+#   Button
+item_number_button = tk.Button(text="Enter", font=("arial", 10), command=to_do)
+item_number_button.place(x=30, y=75)
+y_shift = 0
+counter = 0
+
+
+window.mainloop()

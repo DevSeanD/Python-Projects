479f19ebb24eb6eccb89bfb2182fd1ef89eb8dcc
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2019-05-29 17:44:38 -0400

    Added the correct time intervals

--- Pomodoro_Timer.py
+++ Pomodoro_Timer.py
@@ -39,25 +39,33 @@ def Sixth_Study_Sesh():
     label_sixth_sesh = tk.Label(text="Break 3 is done! Study Session 4 begins now!")
     label_sixth_sesh.pack(side="top")
 
+def Seventh_Study_Sesh():
+    label_seventh_sesh = tk.Label(text="Pomodoro Session Complete!")
+    label_seventh_sesh.pack(side="top")
+
 
 def Timer_Start():
-    timer_start = threading.Timer(10.0,First_Study_Sesh) # after 10 seconds function First_Study_Sesh will run
+    timer_start = threading.Timer(1500.0,First_Study_Sesh) # after 10 seconds function First_Study_Sesh will run
     timer_start.start()
 
-    timer_start_1 = threading.Timer(15.0, Second_Study_Sesh) # after 15 seconds function Break_Five will run
+    timer_start_1 = threading.Timer(1800.0, Second_Study_Sesh) # after 15 seconds function Break_Five will run
     timer_start_1.start()
 
-    timer_start_2 = threading.Timer(25.0, Third_Study_Sesh) # after 25 seconds function Third_study_sesh will run
+    timer_start_2 = threading.Timer(3300.0, Third_Study_Sesh) # after 25 seconds function Third_study_sesh will run
     timer_start_2.start()
 
-    timer_start_3 = threading.Timer(30.0, Fourth_Study_Sesh) # after 15 seconds function Fourth_study_sesh will run 
+    timer_start_3 = threading.Timer(3600.0, Fourth_Study_Sesh) # after 15 seconds function Fourth_study_sesh will run 
     timer_start_3.start()
 
-    timer_start_4 = threading.Timer(40.0, Fifth_Study_Sesh)
+    timer_start_4 = threading.Timer(3600.0, Fifth_Study_Sesh)
     timer_start_4.start()
 
-    timer_start_5 = threading.Timer(45.0, Sixth_Study_Sesh)
+    timer_start_5 = threading.Timer(5100.0, Sixth_Study_Sesh)
     timer_start_5.start()
+
+    timer_start_6 = threading.Timer(5400.0, Seventh_Study_Sesh)
+    timer_start_6.start()
+
 # Lables
 label_welcome = tk.Label(text="Welcome to Pomodoro Timer!",bd=1)
 label_welcome.pack(side="top")
@@ -65,7 +73,3 @@ label_welcome.pack(side="top")
 #Buttons
 start_button = tk.Button(text="Start Timer", command=Timer_Start)
 start_button.pack(side="top")
-
-
-
-window.mainloop()

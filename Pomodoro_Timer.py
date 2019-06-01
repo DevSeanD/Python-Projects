39ea90d32a6be85881e2fdc3de4c5a30ece477e5
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2019-06-01 14:43:47 -0400

    Added self checking checkboxes
    The check boxes that are supplied to the user will now check themselves when the entire pomodoro cycle is completed

--- Pomodoro_Timer.py
+++ Pomodoro_Timer.py
@@ -13,6 +13,18 @@ window = tk.Tk()
 window.title("Pomodoro Timer")
 window.geometry("500x500")
 
+counter = 0
+ 
+check_val_1 = tk.BooleanVar()
+check_val_1.set(True)
+
+check_val_2 = tk.BooleanVar()
+check_val_2.set(True)
+
+check_val_3 = tk.BooleanVar()
+check_val_3.set(True)
+
+
 def First_Study_Sesh():
     label_blank = tk.Label(text=" ")
     label_blank.pack(side="top")
@@ -40,27 +52,27 @@ def Sixth_Study_Sesh():
     label_sixth_sesh.pack(side="top")
 
 def Seventh_Study_Sesh():
-    label_seventh_sesh = tk.Label(text="Pomodoro Session Complete!")
+    label_seventh_sesh = tk.Label(text="Pomodoro Session Complete!(25 min break)")
     label_seventh_sesh.pack(side="top")
 
-def Timer_Start():
-    yshift = 135
-    number_seshs = 0
+def Check_box_1_set():
+    check_box_complete = tk.Checkbutton(variable=check_val_1).place(x=120,y=135)
 
+def Check_box_2_set():
+    check_box_complete_2 = tk.Checkbutton(variable=check_val_2).place(x=120,y=315)
+
+def Check_box_3_set():
+    check_box_complete_3 = tk.Checkbutton(variable=check_val_3).place(x=120,y=490)
+
+def Timer_Start(): 
     label_sesh_1= tk.Label(text="Session 1")
     label_sesh_1.place(x=50,y=135)
-    check_box_1 = tk.Checkbutton()
-    check_box_1.place(x=120,y=135)
-            
+
     label_seshs_2 = tk.Label(text="Session 2")
-    label_seshs_2.place(x=50,y=325)
-    check_box_2 = tk.Checkbutton()
-    check_box_2.place(x=120,y=325)
+    label_seshs_2.place(x=50,y=315)
 
     label_seshs_3 = tk.Label(text="Session 3")
-    label_seshs_3.place(x=50,y=515)
-    check_box_1 = tk.Checkbutton()
-    check_box_1.place(x=120,y=515)
+    label_seshs_3.place(x=50,y=490)
 
     timer_start = threading.Timer(00.0,First_Study_Sesh) # after 10 seconds function First_Study_Sesh will resourcewarning
     timer_start.start()
@@ -82,10 +94,22 @@ def Timer_Start():
  
     timer_start_6 = threading.Timer(06.0, Seventh_Study_Sesh)
     timer_start_6.start()
-            
+
     label_blank_1 = tk.Label(text=" ")
     label_blank_1.pack(side="top")
-
+    
+    global counter
+    counter += 1
+    
+    if counter == 1:
+        check_button_1 = threading.Timer(07.0, Check_box_1_set)
+        check_button_1.start()
+    if counter == 2:
+        check_button_2 =threading.Timer(07.0, Check_box_2_set)
+        check_button_2.start()
+    if counter == 3:
+        check_button_3 = threading.Timer(07.0,Check_box_3_set)
+        check_button_3.start()
 # Lables
 label_welcome = tk.Label(text="Welcome to Pomodoro Timer!",bd=1)
 label_welcome.pack(side="top")
@@ -93,7 +117,6 @@ label_welcome.pack(side="top")
 #Buttons
 start_button = tk.Button(text="Start Timer", command=Timer_Start)
 start_button.pack(side="top")
-
 
 
 window.mainloop()

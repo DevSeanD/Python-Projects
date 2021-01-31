a4f55b15af172183bc66da15cf54f64d5809b69f
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-01-31 12:23:23 -0500

    Added check_box Class

--- Object_Oriented_Bullet_Journal.py
+++ Object_Oriented_Bullet_Journal.py
@@ -13,27 +13,31 @@ window.title("Bullet Journal")
 window.geometry("625x400")
 
 class entry_box():
-    def __init__(self,row_val,col_val,str_val):
+    def __init__(self,row_val,col_val,str_val=None):
+        if str_val is None:
+            str_val = StringVar(value="")
+        else:
+            str_val = StringVar(value=str_val)
         self.entry = tk.Entry(width="60",textvariable=str_val)
         self.entry.grid(row=row_val,column=col_val)
         
 class check_box():
     def __init__(self,row_val,col_val,check_state=None):
         if check_state is None:
-            check_state = IntVar()
-
-        self.check = tk.Checkbutton(var=check_state)
+           self.check_state = IntVar()
+        if check_state == 1:
+            self.check_state = IntVar(value=1)
+        self.check = tk.Checkbutton(var=self.check_state)
         self.check.grid(row=row_val,column=col_val)
-
 
 def save():
     pass
 
 
 check_box_1 = check_box(1,0)
+check_box_2 = check_box(2,0,1)
 
-str_var_1 = StringVar(value="Test")
-entry_1 = entry_box(1,1,str_var_1)
-
+entry_1 = entry_box(1,1)
+entry_2 = entry_box(2,1,"Test")
 
 window.mainloop()

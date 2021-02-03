a9e18c538230bf19c632347de2da8f496f6ed542
DevSeanD <39039620+DevSeanD@users.noreply.github.com>
2021-02-03 08:52:06 -0500

    Added Label Class

--- Object_Oriented_Bullet_Journal.py
+++ Object_Oriented_Bullet_Journal.py
@@ -30,12 +30,41 @@ class check_box():
         self.check = tk.Checkbutton(var=self.check_state)
         self.check.grid(row=row_val,column=col_val)
 
+class label():
+    def __init__(self,str_val,row_val,col_val):
+        sectionTitle = StringVar(value=str_val)
+        sectionTitleLabel = tk.Label(textvariable=sectionTitle)
+        sectionTitleLabel.grid(row=row_val,column=col_val)
+
+
 def save():
     pass
 
 
+routineLabel = label("Routine",0,1)
+schoolLabel = label("School",14,1)
+selfLabel = label("Self",23,1)
+
 check_box_1 = check_box(1,0)
 check_box_2 = check_box(2,0,1)
+check_box_3 = check_box(3,0)
+check_box_4 = check_box(4,0)
+check_box_5 = check_box(5,0)
+check_box_6 = check_box(7,0)
+check_box_7 = check_box(8,0)
+check_box_8 = check_box(9,0)
+check_box_9 = check_box(10,0)
+check_box_10 = check_box(11,0)
+check_box_11 = check_box(12,0)
+check_box_12 = check_box(13,0)
+check_box_13 = check_box(15,0)
+check_box_14 = check_box(16,0)
+check_box_15 = check_box(17,0)
+check_box_16 = check_box(18,0)
+check_box_17 = check_box(19,0)
+check_box_18 = check_box(20,0)
+check_box_19 = check_box(21,0)
+check_box_20 = check_box(22,0)
 
 entry_1 = entry_box(1,1)
 entry_2 = entry_box(2,1,"Test")
